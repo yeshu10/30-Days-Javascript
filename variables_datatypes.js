67cd7da6fbd04e4d2c2d5cbd2b99b0c337fefc32
yeshu10 <yeshvigupta10@gmail.com>
2024-07-13 16:20:28 +0530

    day 1 complete

---
+++ variables_datatypes.js
@@ -0,0 +1,37 @@
+// Variable declaration
+var a=10;
+console.log(a);
+let str="DAYONEOFCHALLENGE";
+console.log(str);
+
+// Const declaration
+const res=true;
+console.log(res);
+
+// DayaTypes
+let num=10;
+let str1="Hello";
+let bool=true;
+let obj={
+    name:"Jack",
+    age:30
+}
+let arr=[1,2,3,4,5];
+
+console.log(typeof num);
+console.log(typeof str1);
+console.log(typeof bool);
+console.log(typeof obj);
+console.log(typeof arr);
+
+//Reassigning Variables
+let b=20;
+console.log(b);
+b=30;
+console.log(b);
+
+// const reassignment
+const c=40;
+console.log(c);
+c=50;
+console.log(c);
