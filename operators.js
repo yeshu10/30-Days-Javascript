652cd9225083eb251cbe34b2b03e476f66c18314
yeshu10 <yeshvigupta10@gmail.com>
2024-07-14 12:34:25 +0530

    Day 2 operators

---
+++ operators.js
@@ -0,0 +1,67 @@
+//Task 1
+let a=10;
+let b=20;
+console.log("Sum is",a+b)
+
+//Task 2
+console.log("b-a is ",b-a)
+
+//Task 3
+console.log("a*b is ",a*b)
+
+//Task 4
+console.log("b/a is ",b/a)
+
+//Task 5
+console.log("Modulus of a and b is ",a%b)
+
+//Task 6
+a+=10;
+console.log("a after increment is ",a)
+
+//Task 7
+b-=10;
+console.log("b after decrement is ",b)
+
+//Task 8
+if(a>b)
+{
+    console.log("a is greater than b")
+}
+else if( a<b)
+{
+    console.log("b is greater than a")
+}
+
+//Task 9
+if(a>=b)
+{
+    console.log("a is equal to or greater than b")
+}
+else if (a<=b)
+{
+ console.log("a is less than or equal to b")   
+}
+
+//Task 10
+console.log(a==b);
+console.log(a===b);
+
+//Task 11
+if(a>10 && a>b)
+{
+    console.log("a is greater than 10 and greater than b")
+}
+
+//Task 12
+if(a==20 || a>10){
+    console.log("a is 20 or greater than 10")
+}
+
+//Task 13
+if(a!=10){
+    console.log("a is not equal to 10")
+}
+
+//Task 14
+(a>0) ? console.log("a is positive") : console.log("a is negative");
